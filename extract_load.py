6b134cc857229e1457ffe3cab8563d337b277d8f
Andy Crowe <mcrowe@samhealth.org>
2020-04-23 16:34:22 -0700

    added multiprocessing

--- extract_load.py
+++ extract_load.py
@@ -4,8 +4,10 @@ import pyodbc
 import pandas as pd
 import json
 import logging
-import queue
+from queue import Queue
 import threading
+import multiprocessing
+from collections import namedtuple
 
 from targets.source_sql_server_target import SourceSqlServerTarget
 from targets.destination_sql_server_target import DestinationSqlServerTarget
@@ -15,33 +17,14 @@ from targets.destination_sql_server_target import DestinationSqlServerTarget
 # print(json)
 
 #TODO: add logging
-def main(configuration_path=None):
-    configuration = None
+def process_source_start():
+    print("start")
 
-    if not configuration_path == None: 
-        with open(configuration_path, "rb") as file:
-            try: 
-                configuration = json.loads(file.read())
-            except Exception as ex: 
-                print(f'extract_load.main: Failed loading configuration {configuration_path}.')
-                print(f'extract_load.main: {ex}')
+def process_source_target(input):
+    
+    source_target = SourceSqlServerTarget(input[0], input[1], input[2], input[3])
+    destination_target = DestinationSqlServerTarget(input[4]["server"], input[4]["database"], input[4]["schema"], input[4]["load_psa"])
 
-    source_targets = []
-    destination_target = DestinationSqlServerTarget(configuration["destination"]["server"], configuration["destination"]["database"], configuration["destination"]["schema"], configuration["destination"]["load_psa"])
-
-    for target in configuration["source"]:
-        for table in target["tables"]:
-            source_targets.append(SourceSqlServerTarget(target["server"], target["database"], target["schema"], table))
-
-    for target in source_targets:
-        process_source_target(target, destination_target)
-
-    #     # get records new thread which loads the queue 
-    #     get_record_thread = threading.Thread(source_target.get_records())
-    #     get_record_thread.join()
-
-
-def process_source_target(source_target, destination_target):
     # check change tracking, if change tracking is not enabled add it 
     if not source_target.check_change_tracking(): 
         source_target.add_change_tracking()
@@ -54,8 +37,8 @@ def process_source_target(source_target, destination_target):
     previous_change_version = destination_target.get_previous_change_tracking_key(source_target.get_table_name, source_target.get_database_name)
 
     # check that the destination tables exist, both stage and psa - if either doesn't exist create them
-    if not destination_target.check_stg_destination_table(source_target.get_table_name, source_target.get_database) and destination_target.check_psa_destination_table(source_target.get_table_name, source_target.get_database):
-        destination_target.create_destination_table(source_target.get_table_name, source_target.get_database)
+    if not destination_target.check_stg_destination_table(source_target.get_table_name, source_target.get_database_name) and destination_target.check_psa_destination_table(source_target.get_table_name, source_target.get_database_name):
+        destination_target.create_destination_table(source_target.get_table_name, source_target.get_database_name)
 
 
 if __name__ == "__main__":
@@ -63,6 +46,30 @@ if __name__ == "__main__":
         if sys.argv[1] != None:
             configuration_path = sys.argv[1]
     else:
-        configuration_path = "E:\code\extract_load\configuration.json"
+        configuration_path = r"E:\code\extract_load\configuration.json"
 
-    main(configuration_path)
+    if not configuration_path == None: 
+        with open(configuration_path, "rb") as file:
+            try: 
+                configuration = json.loads(file.read())
+            except Exception as ex: 
+                print(f'extract_load.main: Failed loading configuration {configuration_path}.')
+                print(f'extract_load.main: {ex}')
+
+    # sets max concurrent processes based on configuration else defaults to 4
+    pool_size = 1 #multiprocessing.cpu_count() * 2
+    pool = multiprocessing.Pool(pool_size, initializer=process_source_start)
+    
+    source_target_processes = []
+    data = list()
+
+    for target in configuration["source"]:
+        for table in target["tables"]:
+            data.append((target["server"], target["database"], target["schema"], table, configuration["destination"]))
+            # create each individual processes for each source target, we can then throttle based on machine cores. Arguements must be picklable (serializable/deserializable)
+            # source_target_processes.append(multiprocessing.Process(target=process_source_target, args=(target["server"], target["database"], target["schema"], table, configuration["destination"]), daemon=True))
+    
+    pool.map(process_source_target, data)
+    pool.close()
+    pool.join()
+    
--- unit_tests/test_destination_sql_server_target.py
+++ unit_tests/test_destination_sql_server_target.py
@@ -11,8 +11,8 @@ from targets.destination_sql_server_target import DestinationSqlServerTarget
 class TestDestinationSqlServerTarget(unittest.TestCase):
     
     def setUp(self):
-        self._configuration = json.loads('{ "server" : "DEVSQL17TRZRP", "database" : "hpXr_Stage" }')
-        self._destination_sql_server_target = DestinationSqlServerTarget(self._configuration)
+        self._configuration = json.loads('{ "server" : "DEVSQL17TRZRP", "database" : "hpXr_Stage", "schema" : "dbo" }')
+        self._destination_sql_server_target = DestinationSqlServerTarget(self._configuration["server"], self._configuration["database"], self._configuration["schema"], self._configuration["load_psa"])
 
     def test_check_psa_destination_table(self):
         self.assertTrue(self._destination_sql_server_target.check_psa_destination_table("TEST_CMC_CLCL_CLAIM", "facets"))
@@ -28,35 +28,3 @@ class TestDestinationSqlServerTarget(unittest.TestCase):
 
     def test_get_previous_change_tracking_key(self):
         self.assertIsNotNone(self._destination_sql_server_target.get_previous_change_tracking_key("TEST_CMC_CLCL_CLAIM", "facets"))
-
-    def test_exec_sp(self):
-        server = 'DEVSQL17TRZ3'
-        database = 'facets'
-        # connection_string =f"DRIVER={{ODBC Driver 17 for SQL Server}}; SERVER=DEVSQL17TRZRP; DATEBASE=hpXr_Stage; Trusted_Connection=yes"
-        connection_string = 'DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+'; Trusted_Connection=yes'
-        conn = pyodbc.connect(connection_string)
-        # create_stg_table = f"EXEC hpXr_Stage.[dbo].[SHS_SP_CREATE_STG_TABLE] @table_name = ?, @source_database_name = ?"
-        # params = ("TEST_CMC_CLCL_CLAIM", "facets")
-        sql = "SELECT * FROM CMC_CLCL_CLAIM"
-        crsr = conn.cursor()
-        
-        # ret = crsr.execute("SELECT * FROM CMC_CLCL_CLAIM").fetchall()
-        # val = ret
-        
-        for row in crsr.execute(sql):
-            print(row)
-        
-        # df = pd.read_sql(sql, conn)
-        # json = df.loc[0].to_json()
-        # print(json)
-
-        # pd.to_sql()
-
-        # crsr = conn.cursor()
-        # try:
-        #     crsr.execute("{CALL hpXr_Stage.dbo.SHS_SP_CREATE_STG_TABLE (?,?)}", params)
-        #     crsr.commit()
-        # except:
-        #     print("error")            
-        # ret1 = cursor.execute(create_stg_table, create_stg_table_params)
-
